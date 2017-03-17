18c7a4d56e15036ae4e0c1ed33782b0e1912cfda
Lennert Gijsen <l.gijsen@gijsenwebdesign.nl>
2017-03-17 16:46:44 +0100

    Task 4.7

---
+++ numbers-service/src/main/java/nl/johannisk/cloud/numbersservice/controller/FibonacciNumbersResponse.java
@@ -0,0 +1,28 @@
+package nl.johannisk.cloud.numbersservice.controller;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class FibonacciNumbersResponse {
+
+
+    private final List<Integer> fibonacciNumbers;
+    private final String instanceId;
+
+    @JsonCreator
+    public FibonacciNumbersResponse(@JsonProperty("fibonacciNumbers") List<Integer> fibonacciNumbers,
+                                    @JsonProperty("instanceId") String instanceId) {
+        this.fibonacciNumbers = fibonacciNumbers;
+        this.instanceId = instanceId;
+    }
+
+    public List<Integer> getFibonacciNumbers() {
+        return fibonacciNumbers;
+    }
+
+    public String getInstanceId() {
+        return instanceId;
+    }
+}
