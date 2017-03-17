66df558fe819d9bc09b4d18fd69a72ba51cee0cc
Lennert Gijsen <l.gijsen@gijsenwebdesign.nl>
2017-03-17 16:42:23 +0100

    Task 4.3

--- numbers-service/src/test/resources/contracts/returnPrimesFrom1to10.groovy
+++ numbers-service/src/test/resources/contracts/returnPrimesFrom1to10.groovy
@@ -3,22 +3,24 @@ package contracts;
 org.springframework.cloud.contract.spec.Contract.make {
     request {
         method 'POST'
-        url '/primenumbers'
-        body([
-                from:1,
-                to:10
-        ])
+        url '/fibonaccinumbers'
+        body(
+                [from:1,
+                 to:10]
+        )
         headers {
             header('Content-Type', 'application/json;charset=UTF-8')
         }
     }
     response {
         status 200
-        body([
-                primeNumbers: [2,3,5,7],
-                instanceId: value(consumer('numbers-service-005887b1ff2a0c4cf0b94414d6e74a3a'),
-                        producer(regex('numbers-service-[a-fA-F0-9]{32}')))
-        ])
+        body(
+                [
+                        fibonacciNumbers: [1, 1, 2, 3, 5, 8],
+                        instanceId  : value(consumer('numbers-service-005887b1ff2a0c4cf0b94414d6e74a3a'),
+                                producer(regex('numbers-service-[a-fA-F0-9]{32}')))
+                ]
+        )
         headers {
             header('Content-Type': 'application/json;charset=UTF-8')
         }
